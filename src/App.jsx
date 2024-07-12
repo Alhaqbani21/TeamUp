3d67edfb576f2c0f0c7b281ddd828ea9ab4dbecd
Abdulaziz Alhaqbani <alhaqbani21@gmail.com>
2024-07-12 14:25:43 +0300

    init files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Router from './routes/Router';
+
+function App() {
+  return (
+    <>
+      <Router />
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes/Router.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from '../pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+]);
+function Router() {
+  return <RouterProvider router={router} />;
+}
+
+export default Router;

47f665a1b4c25f4b3f14d6f4cb7a9e727bd674d1
ammalgh <ammalgh22@gmail.com>
2024-07-18 22:07:32 +0300

    update

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,19 +1,19 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { auth, db } from '../config/firebase';
+import { Link, useNavigate } from "react-router-dom";
+import { auth, db } from "../config/firebase";
 import {
   createUserWithEmailAndPassword,
   GoogleAuthProvider,
   signInWithPopup,
-} from 'firebase/auth';
-import { useState } from 'react';
-import { setDoc, doc, getDoc } from 'firebase/firestore';
+} from "firebase/auth";
+import { useState } from "react";
+import { setDoc, doc, getDoc } from "firebase/firestore";
 
 // import image1 from "../assets/image1.jpg";
 
 function SignUp() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   async function signUpEmailAndPassword() {
@@ -27,24 +27,24 @@ function SignUp() {
         const user = userCredential.user;
 
         // Add user to users collection
-        await setDoc(doc(db, 'users', user.uid), {
+        await setDoc(doc(db, "users", user.uid), {
           name: name,
           email: email,
           joinedAt: formatDate(new Date().toISOString()),
-          location: 'Saudi Arabia, Riyadh',
+          location: "Saudi Arabia, Riyadh",
           points: 0,
           matchesPlayed: 0,
         });
 
         // Add user to leaderboard collection
-        await setDoc(doc(db, 'leaderboard', user.uid), {
+        await setDoc(doc(db, "leaderboard", user.uid), {
           name: name,
           points: 0,
         });
 
-        navigate('../login');
+        navigate("../login");
       } catch (error) {
-        console.error('Error signing up: ', error);
+        console.error("Error signing up: ", error);
         // Handle errors appropriately
       }
     }
@@ -57,47 +57,47 @@ function SignUp() {
       const user = result.user;
 
       // Check if the user document exists in users collection
-      const userDoc = await getDoc(doc(db, 'users', user.uid));
+      const userDoc = await getDoc(doc(db, "users", user.uid));
       if (!userDoc.exists()) {
         // Add user to users collection
-        await setDoc(doc(db, 'users', user.uid), {
+        await setDoc(doc(db, "users", user.uid), {
           name: user.displayName,
           email: user.email,
           joinedAt: formatDate(new Date().toISOString()),
-          location: 'Saudi Arabia, Riyadh',
+          location: "Saudi Arabia, Riyadh",
           points: 0,
           matchesPlayed: 0,
         });
       }
 
       // Check if the user document exists in leaderboard collection
-      const leaderboardDoc = await getDoc(doc(db, 'leaderboard', user.uid));
+      const leaderboardDoc = await getDoc(doc(db, "leaderboard", user.uid));
       if (!leaderboardDoc.exists()) {
         // Add user to leaderboard collection
-        await setDoc(doc(db, 'leaderboard', user.uid), {
+        await setDoc(doc(db, "leaderboard", user.uid), {
           name: user.displayName,
           points: 0,
         });
       }
 
-      navigate('../Home');
+      navigate("../Home");
     } catch (error) {
-      console.error('Error signing up with Google: ', error);
+      console.error("Error signing up with Google: ", error);
       // Handle errors appropriately
     }
   }
 
   function formatDate(dateString) {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const options = { year: "numeric", month: "long", day: "numeric" };
     return new Date(dateString).toLocaleDateString(undefined, options);
   }
 
   return (
     <>
       {/* component */}
-      <div className="bg-white relative lg:py-20 h-screen">
+      <div className="bg-white relative lg:py-20 ">
         <div
-          className="flex flex-col items-center justify-between pt-0 pr-10 pb-0 pl-10 mt-0 mr-auto mb-0 ml-auto max-w-7xl
+          className="flex flex-col items-center justify-between pt-0 pr-10 pb-0 pl-10 mt-0 mr-auto mb-0 ml-auto 
       xl:px-5 lg:flex-row"
         >
           <div className="flex flex-col items-center w-full pt-5 pr-10 pb-20 pl-10 lg:pt-20 lg:flex-row">
@@ -167,6 +167,12 @@ function SignUp() {
                     />
                   </div>
                   <div className="relative">
+                    <p className="text-left text-sm mb-3">
+                      Do you have an account?{" "}
+                      <Link className="text-blue-500 underline" to={"../login"}>
+                        Login
+                      </Link>
+                    </p>
                     <div
                       onClick={signUpEmailAndPassword}
                       className="cursor-pointer w-full inline-block pt-4 pr-5 pb-4 pl-5 text-xl font-medium text-center text-white bg-[#007955]
@@ -174,206 +180,21 @@ function SignUp() {
                     >
                       SignUp
                     </div>
+                    <div className="my-10 border-b text-center">
+                      <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
+                        Or Sign Up with Google
+                      </div>
+                    </div>
                     <div
                       onClick={signUpGoogle}
-                      className="cursor-pointer my-3 w-full inline-block pt-4 pr-5 pb-4 pl-5 text-xl font-medium text-center text-white bg-[#007955]
+                      className="cursor-pointer my-3 w-full inline-block pt-4 pr-5 pb-4 pl-5 text-xl font-medium text-center text-white bg-[#83afa2]
                   rounded-lg transition duration-200 hover:bg-[#215244]"
                     >
                       Google
                     </div>
-                    <p className="text-center text-sm mt-2">
-                      Do you have an account?{' '}
-                      <Link className="text-blue-500 underline" to={'../login'}>
-                        Login
-                      </Link>
-                    </p>
                   </div>
                 </div>
               </div>
-              {/* <svg
-                viewBox="0 0 91 91"
-                className="absolute top-0 left-0 z-0 w-32 h-32 -mt-12 -ml-12 text-[#91B3FA]
-            fill-current"
-              >
-                <g stroke="none" strokeWidth={1} fillRule="evenodd">
-                  <g fillRule="nonzero">
-                    <g>
-                      <g>
-                        <circle cx="3.261" cy="3.445" r="2.72" />
-                        <circle cx="15.296" cy="3.445" r="2.719" />
-                        <circle cx="27.333" cy="3.445" r="2.72" />
-                        <circle cx="39.369" cy="3.445" r="2.72" />
-                        <circle cx="51.405" cy="3.445" r="2.72" />
-                        <circle cx="63.441" cy="3.445" r="2.72" />
-                        <circle cx="75.479" cy="3.445" r="2.72" />
-                        <circle cx="87.514" cy="3.445" r="2.719" />
-                      </g>
-                      <g transform="translate(0 12)">
-                        <circle cx="3.261" cy="3.525" r="2.72" />
-                        <circle cx="15.296" cy="3.525" r="2.719" />
-                        <circle cx="27.333" cy="3.525" r="2.72" />
-                        <circle cx="39.369" cy="3.525" r="2.72" />
-                        <circle cx="51.405" cy="3.525" r="2.72" />
-                        <circle cx="63.441" cy="3.525" r="2.72" />
-                        <circle cx="75.479" cy="3.525" r="2.72" />
-                        <circle cx="87.514" cy="3.525" r="2.719" />
-                      </g>
-                      <g transform="translate(0 24)">
-                        <circle cx="3.261" cy="3.605" r="2.72" />
-                        <circle cx="15.296" cy="3.605" r="2.719" />
-                        <circle cx="27.333" cy="3.605" r="2.72" />
-                        <circle cx="39.369" cy="3.605" r="2.72" />
-                        <circle cx="51.405" cy="3.605" r="2.72" />
-                        <circle cx="63.441" cy="3.605" r="2.72" />
-                        <circle cx="75.479" cy="3.605" r="2.72" />
-                        <circle cx="87.514" cy="3.605" r="2.719" />
-                      </g>
-                      <g transform="translate(0 36)">
-                        <circle cx="3.261" cy="3.686" r="2.72" />
-                        <circle cx="15.296" cy="3.686" r="2.719" />
-                        <circle cx="27.333" cy="3.686" r="2.72" />
-                        <circle cx="39.369" cy="3.686" r="2.72" />
-                        <circle cx="51.405" cy="3.686" r="2.72" />
-                        <circle cx="63.441" cy="3.686" r="2.72" />
-                        <circle cx="75.479" cy="3.686" r="2.72" />
-                        <circle cx="87.514" cy="3.686" r="2.719" />
-                      </g>
-                      <g transform="translate(0 49)">
-                        <circle cx="3.261" cy="2.767" r="2.72" />
-                        <circle cx="15.296" cy="2.767" r="2.719" />
-                        <circle cx="27.333" cy="2.767" r="2.72" />
-                        <circle cx="39.369" cy="2.767" r="2.72" />
-                        <circle cx="51.405" cy="2.767" r="2.72" />
-                        <circle cx="63.441" cy="2.767" r="2.72" />
-                        <circle cx="75.479" cy="2.767" r="2.72" />
-                        <circle cx="87.514" cy="2.767" r="2.719" />
-                      </g>
-                      <g transform="translate(0 61)">
-                        <circle cx="3.261" cy="2.846" r="2.72" />
-                        <circle cx="15.296" cy="2.846" r="2.719" />
-                        <circle cx="27.333" cy="2.846" r="2.72" />
-                        <circle cx="39.369" cy="2.846" r="2.72" />
-                        <circle cx="51.405" cy="2.846" r="2.72" />
-                        <circle cx="63.441" cy="2.846" r="2.72" />
-                        <circle cx="75.479" cy="2.846" r="2.72" />
-                        <circle cx="87.514" cy="2.846" r="2.719" />
-                      </g>
-                      <g transform="translate(0 73)">
-                        <circle cx="3.261" cy="2.926" r="2.72" />
-                        <circle cx="15.296" cy="2.926" r="2.719" />
-                        <circle cx="27.333" cy="2.926" r="2.72" />
-                        <circle cx="39.369" cy="2.926" r="2.72" />
-                        <circle cx="51.405" cy="2.926" r="2.72" />
-                        <circle cx="63.441" cy="2.926" r="2.72" />
-                        <circle cx="75.479" cy="2.926" r="2.72" />
-                        <circle cx="87.514" cy="2.926" r="2.719" />
-                      </g>
-                      <g transform="translate(0 85)">
-                        <circle cx="3.261" cy="3.006" r="2.72" />
-                        <circle cx="15.296" cy="3.006" r="2.719" />
-                        <circle cx="27.333" cy="3.006" r="2.72" />
-                        <circle cx="39.369" cy="3.006" r="2.72" />
-                        <circle cx="51.405" cy="3.006" r="2.72" />
-                        <circle cx="63.441" cy="3.006" r="2.72" />
-                        <circle cx="75.479" cy="3.006" r="2.72" />
-                        <circle cx="87.514" cy="3.006" r="2.719" />
-                      </g>
-                    </g>
-                  </g>
-                </g>
-              </svg>
-              <svg
-                viewBox="0 0 91 91"
-                className="absolute bottom-0 right-0 z-0 w-32 h-32 -mb-12 -mr-12 text-[#007955]
-            fill-current"
-              >
-                <g stroke="none" strokeWidth={1} fillRule="evenodd">
-                  <g fillRule="nonzero">
-                    <g>
-                      <g>
-                        <circle cx="3.261" cy="3.445" r="2.72" />
-                        <circle cx="15.296" cy="3.445" r="2.719" />
-                        <circle cx="27.333" cy="3.445" r="2.72" />
-                        <circle cx="39.369" cy="3.445" r="2.72" />
-                        <circle cx="51.405" cy="3.445" r="2.72" />
-                        <circle cx="63.441" cy="3.445" r="2.72" />
-                        <circle cx="75.479" cy="3.445" r="2.72" />
-                        <circle cx="87.514" cy="3.445" r="2.719" />
-                      </g>
-                      <g transform="translate(0 12)">
-                        <circle cx="3.261" cy="3.525" r="2.72" />
-                        <circle cx="15.296" cy="3.525" r="2.719" />
-                        <circle cx="27.333" cy="3.525" r="2.72" />
-                        <circle cx="39.369" cy="3.525" r="2.72" />
-                        <circle cx="51.405" cy="3.525" r="2.72" />
-                        <circle cx="63.441" cy="3.525" r="2.72" />
-                        <circle cx="75.479" cy="3.525" r="2.72" />
-                        <circle cx="87.514" cy="3.525" r="2.719" />
-                      </g>
-                      <g transform="translate(0 24)">
-                        <circle cx="3.261" cy="3.605" r="2.72" />
-                        <circle cx="15.296" cy="3.605" r="2.719" />
-                        <circle cx="27.333" cy="3.605" r="2.72" />
-                        <circle cx="39.369" cy="3.605" r="2.72" />
-                        <circle cx="51.405" cy="3.605" r="2.72" />
-                        <circle cx="63.441" cy="3.605" r="2.72" />
-                        <circle cx="75.479" cy="3.605" r="2.72" />
-                        <circle cx="87.514" cy="3.605" r="2.719" />
-                      </g>
-                      <g transform="translate(0 36)">
-                        <circle cx="3.261" cy="3.686" r="2.72" />
-                        <circle cx="15.296" cy="3.686" r="2.719" />
-                        <circle cx="27.333" cy="3.686" r="2.72" />
-                        <circle cx="39.369" cy="3.686" r="2.72" />
-                        <circle cx="51.405" cy="3.686" r="2.72" />
-                        <circle cx="63.441" cy="3.686" r="2.72" />
-                        <circle cx="75.479" cy="3.686" r="2.72" />
-                        <circle cx="87.514" cy="3.686" r="2.719" />
-                      </g>
-                      <g transform="translate(0 49)">
-                        <circle cx="3.261" cy="2.767" r="2.72" />
-                        <circle cx="15.296" cy="2.767" r="2.719" />
-                        <circle cx="27.333" cy="2.767" r="2.72" />
-                        <circle cx="39.369" cy="2.767" r="2.72" />
-                        <circle cx="51.405" cy="2.767" r="2.72" />
-                        <circle cx="63.441" cy="2.767" r="2.72" />
-                        <circle cx="75.479" cy="2.767" r="2.72" />
-                        <circle cx="87.514" cy="2.767" r="2.719" />
-                      </g>
-                      <g transform="translate(0 61)">
-                        <circle cx="3.261" cy="2.846" r="2.72" />
-                        <circle cx="15.296" cy="2.846" r="2.719" />
-                        <circle cx="27.333" cy="2.846" r="2.72" />
-                        <circle cx="39.369" cy="2.846" r="2.72" />
-                        <circle cx="51.405" cy="2.846" r="2.72" />
-                        <circle cx="63.441" cy="2.846" r="2.72" />
-                        <circle cx="75.479" cy="2.846" r="2.72" />
-                        <circle cx="87.514" cy="2.846" r="2.719" />
-                      </g>
-                      <g transform="translate(0 73)">
-                        <circle cx="3.261" cy="2.926" r="2.72" />
-                        <circle cx="15.296" cy="2.926" r="2.719" />
-                        <circle cx="27.333" cy="2.926" r="2.72" />
-                        <circle cx="39.369" cy="2.926" r="2.72" />
-                        <circle cx="51.405" cy="2.926" r="2.72" />
-                        <circle cx="63.441" cy="2.926" r="2.72" />
-                        <circle cx="75.479" cy="2.926" r="2.72" />
-                        <circle cx="87.514" cy="2.926" r="2.719" />
-                      </g>
-                      <g transform="translate(0 85)">
-                        <circle cx="3.261" cy="3.006" r="2.72" />
-                        <circle cx="15.296" cy="3.006" r="2.719" />
-                        <circle cx="27.333" cy="3.006" r="2.72" />
-                        <circle cx="39.369" cy="3.006" r="2.72" />
-                        <circle cx="51.405" cy="3.006" r="2.72" />
-                        <circle cx="63.441" cy="3.006" r="2.72" />
-                        <circle cx="75.479" cy="3.006" r="2.72" />
-                        <circle cx="87.514" cy="3.006" r="2.719" />
-                      </g>
-                    </g>
-                  </g>
-                </g>
-              </svg> */}
             </div>
           </div>
         </div>

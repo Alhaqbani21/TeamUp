56576ccc8942415d602db8e811547acd520a1df6
Amwaj <amwaj.1431@outlook.com>
2024-07-18 18:19:07 +0300

    firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,19 +1,4 @@
 // src/firebaseConfig.js
-<<<<<<< HEAD
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-// import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore";
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-=======
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
@@ -26,18 +11,10 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
->>>>>>> cea99c7c3c63242b8a4feea3bdebaaf50497a5ff
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-<<<<<<< HEAD
-const analytics = getAnalytics(app);
-export const db = getFirestore(app);
-// export const storage = getStorage(app);
-export { app, analytics };
-=======
 export const db = getFirestore(app);
 
 export { app };
->>>>>>> cea99c7c3c63242b8a4feea3bdebaaf50497a5ff

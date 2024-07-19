3100a7ea06c197783cada7ffb5ccb0b90355bcfd
ammalgh <ammalgh22@gmail.com>
2024-07-19 17:28:21 +0300

    function Book

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -3,9 +3,24 @@ import SideBar from "../components/SideBar";
 import Cardheder from "../components/Cardheder";
 import { Link } from "react-router-dom";
 import BottomNavBar from "../components/BottomNavBar";
-
-import React from "react";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../config/firebase";
+import { useEffect, useState } from "react";
 function Book() {
+  const [data, setData] = useState([]);
+  const dataBase = collection(db, "stadium");
+  useEffect(() => {
+    const get = async () => {
+      const data = await getDocs(dataBase);
+      const filter = data.docs.map((e) => ({
+        ...e.data(),
+        id: e.id,
+      }));
+      setData(filter);
+    };
+    get();
+  }, []);
+
   return (
     <>
       <div className="h-screen w-full bg-base-100 relative flex overflow-hidden">
@@ -23,44 +38,51 @@ function Book() {
               <h2 className="text-center text-3xl p-2 tracking-widest  text-[#007955]">
                 All Stadiums
               </h2>
-
               <div className="flex space-x-2 justify-center items-center max-sm:flex-col">
                 <div className="grid lg:grid-cols-2 md:grid-cols-2 max-sm:grid-cols-1 gap-8 justify-items-center mt-8 px-4 max-sm:w-[27em] ">
                   {/* c1 */}
-                  <Fade direction="right">
-                    <div className="bg-[#fffcfc] rounded-md  shadow-2xl  w-[32em]  ">
-                      <Link
-                        className="w-full hover:scale-105 transition-all"
-                        to={"/Booking/Volly"}
-                      >
-                        <div className="md:flex px-4 leading-none max-w-4xl   mt-0">
-                          <div className="flex-none">
-                            <img
-                              src="https://i.pinimg.com/474x/5c/93/42/5c9342018257adcbd3583e22de1461d9.jpg"
-                              alt="pic"
-                              className="h-72 w-56 rounded-md shadow-4xl transform -translate-y-4 border-4 border-gray-300"
-                            />
-                          </div>
-                          <div className="flex-col text-black ml-10">
-                            <p className="pt-4 text-xl font-bold">
-                              {" "}
-                              Stad Revolt
-                            </p>
+                  {data.map((e) => {
+                    return (
+                      <div key={e.id}>
+                        <Fade direction="right">
+                          <div className="bg-[#fffcfc] rounded-md  shadow-2xl  w-[32em]  ">
+                            <Link
+                              className="w-full hover:scale-105 transition-all"
+                              to={`/Booking/${e.id}`}
+                            >
+                              <div className="md:flex px-4 leading-none max-w-4xl   mt-0">
+                                <div className="flex-none">
+                                  <img
+                                    src={e.img}
+                                    alt="pic"
+                                    className="h-72 w-56 rounded-md shadow-4xl transform -translate-y-4 border-4 border-gray-300"
+                                  />
+                                </div>
+                                <div className="flex-col text-black ml-10">
+                                  <p className="pt-4 text-xl font-bold">
+                                    {e.name}
+                                  </p>
 
-                            <h1 className="text-gray-800 font-bold mt-10">
-                              $129
-                            </h1>
-                            <h1 className="bg-[#007955] text-xs text-white px-4 py-2 font-semibold rounded uppercase hover:bg-[#6bc1a7] mt-10  inline-block">
-                              <span className="text-white">2 hour</span>
-                            </h1>
-                            <div className="border-t-2 border-gray-800 my-4 w-[14em] inline-block mt-9"></div>
+                                  <h1 className="text-gray-800 font-bold mt-10">
+                                    {e.price}
+                                  </h1>
+                                  <h1 className="bg-[#007955] text-xs text-white px-4 py-2 font-semibold rounded uppercase hover:bg-[#6bc1a7] mt-10  inline-block">
+                                    <span className="text-white">2 hour</span>
+                                  </h1>
+                                  <div className="border-t-2 border-gray-800 my-4 w-[14em] inline-block mt-9"></div>
+                                </div>
+                              </div>
+                            </Link>
                           </div>
-                        </div>
-                      </Link>
-                    </div>
-                  </Fade>
+                        </Fade>
+                      </div>
+                    );
+                  })}
+
+                  {/*  */}
+
                   {/* c2 */}
-                  <Fade direction="right" delay={300}>
+                  {/* <Fade direction="right" delay={300}>
                     <div className="bg-[#fffcfc] rounded-md  shadow-2xl w-[32em] ">
                       <Link
                         className="w-full hover:scale-105 transition-all"
@@ -91,9 +113,9 @@ function Book() {
                         </div>
                       </Link>
                     </div>
-                  </Fade>
+                  </Fade> */}
                   {/* c3 */}
-                  <Fade direction="right" delay={600}>
+                  {/* <Fade direction="right" delay={600}>
                     <div className="bg-[#fffcfc] rounded-md  shadow-2xl w-[32em]  ">
                       <div className="md:flex px-4 leading-none max-w-4xl   mt-0">
                         <div className="flex-none">
@@ -116,9 +138,9 @@ function Book() {
                         </div>
                       </div>
                     </div>
-                  </Fade>
+                  </Fade> */}
                   {/* c4 */}
-                  <Fade direction="right">
+                  {/* <Fade direction="right">
                     <div className="bg-[#fffcfc] rounded-md  shadow-2xl w-[32em]  ">
                       <div className="md:flex px-4 leading-none max-w-4xl   mt-0">
                         <div className="flex-none">
@@ -141,7 +163,7 @@ function Book() {
                         </div>
                       </div>
                     </div>
-                  </Fade>
+                  </Fade> */}
                 </div>
               </div>
             </div>
